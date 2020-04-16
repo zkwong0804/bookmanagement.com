7038d66b303abff8783cd2c7ada9f16daa8c6574
zkwong <zkwong0804@outlook.my>
2020-04-16 10:45:28 +0000

    First commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta 
+        name="viewport" 
+        content="width=device-width, initial-scale=1.0">
+    <title>Book Management System</title>
+</head>
+<body>
+    <h1>Book management system</h1>
+    <h3>from zkwong</h3>
+</body>
+</html>
